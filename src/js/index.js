4751a83e16098233104533b612880c4b0318670e
Franck WANG <franckwang123@gmail.com>
2017-01-01 13:45:26 +0100

    init template

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+// Es6 support
+require("babel-polyfill");
+// Jquery
+window.$ = window.jQuery = require("jquery");
+// Core bootstrap
+require('bootstrap');
+
+// Plugin JavaScript
+require("jquery.easing");
+
+// Agency theme: contact me
+require("agency/js/jqBootstrapValidation.js");
+require("agency/js/contact_me.js");
+// Theme JavaScript
+require("agency/js/agency.min.js");
