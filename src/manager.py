b275137d504b214d00b4eee4cafd8d5e96e73b92
Franck WANG <franckwang123@gmail.com>
2017-01-02 12:09:28 +0100

    can build html from json

---
+++ src/manager.py
@@ -0,0 +1,50 @@
+import json
+
+
+def build_template():
+    path = "src/template/"
+
+    # Read html template
+    with open(path+"grid_block.html") as f_bloc, open(path+"modal.html") as f_modal, open(path+'main.html') as f_main:
+        block_modal = "".join(f_modal.readlines())
+        block_template = "".join(f_bloc.readlines())
+        block_main = ''.join(f_main.readlines())
+
+    # Read Data from json
+    path = 'src/data/'
+    with open(path+"projects.json") as f_proj, open(path+"tuto.json") as f_tuto:
+        projects_data = json.loads("".join(f_proj.readlines()))
+        tuto_data = json.loads("".join(f_tuto.readlines()))
+
+    
+    # Initialize html code for each template
+    all_modal_html = ""
+    all_project_html = ""
+    all_tuto_html = ""
+
+
+    # Build tuto
+    for data in tuto_data.values():
+        all_tuto_html += block_template.format(**data)
+        with open(data["path_modal"]) as ff:
+            modal_html = ''.join(ff.readlines())
+        all_modal_html += block_modal.format(id=data["id"], type="tutorial",
+                                             html=modal_html)
+
+    # Build tuto
+    for data in projects_data.values():
+        all_project_html += block_template.format(**data)
+        with open(data["path_modal"]) as ff:
+            modal_html = ''.join(ff.readlines())
+        all_modal_html += block_modal.format(id=data["id"], type="project",
+                                             html=modal_html)
+
+    # Write html in template
+    html = block_main.format(portfolio_tuto=all_tuto_html,
+                             portfolio_project=all_project_html,
+                             html_modal=all_modal_html)
+    with open("index.html", "w") as ff:
+        ff.write(html)
+
+if __name__ == '__main__':
+    build_template()
